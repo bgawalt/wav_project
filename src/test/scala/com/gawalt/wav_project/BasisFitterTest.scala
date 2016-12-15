c0938de87993e1157296fb6c4a7c8257275fcc1b
Brian Gawalt <bgawalt@gmail.com>
2016-12-14 22:11:48 -0800

    whoops forgot to update the tests to use Arrays instead of Lists for residual snippets

--- src/test/scala/com/gawalt/wav_project/BasisFitterTest.scala
+++ src/test/scala/com/gawalt/wav_project/BasisFitterTest.scala
@@ -40,21 +40,21 @@ class BasisFitterTest extends FunSuite with Matchers {
     val bossRef = TestActorRef[DummyBoss]
     val bfRef = TestActorRef(new BasisFitter(1, bossRef, basis))
     val bf = bfRef.underlyingActor
-    bfRef ! ResidualSnippetMsg(List(2.0, 4.0, 6.0, 8.0))
+    bfRef ! ResidualSnippetMsg(Array(2.0, 4.0, 6.0, 8.0))
     bf.scaleFactor should be (2.0 +- 1e-6)
     bf.varianceReduction should be (120.0 +- 1e-6)
 
-    bfRef ! ResidualSnippetMsg(List(-1.5, -3.0, -4.5, -6.0))
+    bfRef ! ResidualSnippetMsg(Array(-1.5, -3.0, -4.5, -6.0))
     bf.scaleFactor should be (-1.5 +- 1e-6)
     bf.varianceReduction should be (67.5 +- 1e-6)
 
 
-    bfRef ! ResidualSnippetMsg(List(1.0, 1.0, 1.0, 1.0))
+    bfRef ! ResidualSnippetMsg(Array(1.0, 1.0, 1.0, 1.0))
     bf.scaleFactor should be (0.33333333333 +- 1e-6)
     bf.varianceReduction should be (3.3333333 +- 1e-6)
 
     // Check out repeated residual reduction.
-    val original = List(3.0, 4.0, 9.0, 2.0)
+    val original = Array(3.0, 4.0, 9.0, 2.0)
     bfRef ! ResidualSnippetMsg(original)
     bf.scaleFactor should be (1.5333333333333334 +- 1e-6)
     val updated = original.zip(basis).map({case (oi, bi) => oi - 1.5333333333333334*bi})
@@ -68,7 +68,7 @@ class BasisFitterTest extends FunSuite with Matchers {
     val boss = bossRef.underlyingActor
     val bfRef = TestActorRef(new BasisFitter(9, bossRef, basis))
 
-    bfRef ! ResidualSnippetMsg(List(2.0, 4.0, 6.0, 8.0))
+    bfRef ! ResidualSnippetMsg(Array(2.0, 4.0, 6.0, 8.0))
     bfRef ! BasisFitRequest
     var msgs = boss.getReceivedMsgs
     msgs.length should be (1)
@@ -76,7 +76,7 @@ class BasisFitterTest extends FunSuite with Matchers {
     msgs(0).varianceReduction should be (120.0 +- 1e-6)
     msgs(0).scale should be (2.0 +- 1e-6)
 
-    bfRef ! ResidualSnippetMsg(List(1.0, 1.0, 1.0, 1.0))
+    bfRef ! ResidualSnippetMsg(Array(1.0, 1.0, 1.0, 1.0))
     bfRef ! BasisFitRequest
     msgs = boss.getReceivedMsgs
     msgs.length should be (2)
@@ -102,14 +102,14 @@ class BasisFitterTest extends FunSuite with Matchers {
     val bossRef = TestActorRef[DummyBoss]
     val bfRef = TestActorRef(new BasisFitter(9, bossRef, basis))
     val tooLongException = intercept[Exception] {
-      bfRef.receive(ResidualSnippetMsg(List(1.0, 2.0, 3.0, 4.0, 5.0)))
+      bfRef.receive(ResidualSnippetMsg(Array(1.0, 2.0, 3.0, 4.0, 5.0)))
     }
     tooLongException shouldBe a [IllegalArgumentException]
     tooLongException.getMessage should be (
       "requirement failed: residual length (5) does not match basis length (4)")
 
     val tooShortException = intercept[Exception] {
-      bfRef.receive(ResidualSnippetMsg(List(1.0, 2.0, 3.0)))
+      bfRef.receive(ResidualSnippetMsg(Array(1.0, 2.0, 3.0)))
     }
     tooShortException shouldBe a [IllegalArgumentException]
     tooShortException.getMessage should be (
